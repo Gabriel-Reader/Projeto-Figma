59a8cf456ac39820914bac02fce980bc7b72c50c
Renan86 <renanhurtado.pro@gmail.com>
2025-10-23 23:00:52 -0300

    Adicionei Validações de senha e usuario genericas

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+const Usuario_Generico = "admin";
+const Senha_Generica = "Inter";
 
-// Modo escuro
-const darkMode = document.getElementById('darkmodeID');
-const body = document.body;
+const loginFormulario = document.getElementById("loginFormulario");
+const usuarioInput = document.getElementById("usuario");
+const senhaInput = document.getElementById("senha");
+const ulMensagens = document.querySelector(".ul-flashed-messages");
+const liMensagem = document.querySelector(".mensagem-validacao");
 
-darkMode.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
+// Função de validação e envio de formulario
+loginFormulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const usuario = document.getElementById("usuario").value;
+  const senha = document.getElementById("senha").value;
+  liMensagem.textContent = "";
+  ulMensagens.style.display = "none";
+
+  if (usuario === Usuario_Generico && senha === Senha_Generica) {
+    window.location.href = "authorized.html";
+  } else {
+    const mensagemErro = "Usuario ou senha incorretos. Tente novamente.";
+    // Atualiza o conteúdo do flash
+    liMensagem.textContent = mensagemErro;
+    ulMensagens.style.display = "block";
+
+    // Limpa o campo da senha por segurança e foca nele
+    senhaInput.value = "";
+    senhaInput.focus();
+  }
 });
 
-// Redirecionamento do botão Entrar
-const entrarBtn = document.getElementById('btnEntrar');
+// Modo escuro
+const darkMode = document.getElementById("darkmodeID");
+const body = document.body;
 
-entrarBtn.addEventListener('click', () => {
-    window.location.href = '/authorized.html';
+darkMode.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
 });
